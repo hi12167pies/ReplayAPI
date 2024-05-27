be919d16058de5844a8112086ef5e340d5e41ec1
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-05-27 22:15:53 +1000

    recorder

--- src/cf/pies/replay/api/recordable/ReplayRecorder.java
+++ src/cf/pies/replay/api/recordable/ReplayRecorder.java
@@ -5,16 +5,27 @@ import cf.pies.replay.api.recordable.impl.LocationRecordable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+// need a better way of doing this that won't register events
 public class ReplayRecorder implements Listener {
     private final Replay replay;
+    private final JavaPlugin plugin;
 
     public ReplayRecorder(JavaPlugin plugin, Replay replay) {
         this.replay = replay;
+        this.plugin = plugin;
+    }
+
+    public void register() {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    public void unregister() {
+        HandlerList.unregisterAll(this);
     }
 
     @EventHandler(
