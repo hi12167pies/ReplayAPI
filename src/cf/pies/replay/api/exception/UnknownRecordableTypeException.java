d2ab33185de12cd13124c6ad456db728ad115c3c
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-05-29 23:00:46 +1000

    remove exceptions (unused)

--- src/cf/pies/replay/api/exception/UnknownRecordableTypeException.java
+++
@@ -1,7 +0,0 @@
-package cf.pies.replay.api.exception;
-
-public class UnknownRecordableTypeException extends ReplayException {
-    public UnknownRecordableTypeException(int type) {
-        super("Unknown recordable type " + type);
-    }
-}
