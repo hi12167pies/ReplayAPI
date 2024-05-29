68ab5da3b411fff6ee9982c8d09945262a45a93e
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-05-29 20:10:48 +1000

    add place sounds

--- src/cf/pies/replay/api/recordable/world/BlockRecordable.java
+++ src/cf/pies/replay/api/recordable/world/BlockRecordable.java
@@ -4,6 +4,8 @@ import cf.pies.replay.api.Replay;
 import cf.pies.replay.api.ReplayPlayback;
 import cf.pies.replay.api.recordable.Recordable;
 import cf.pies.replay.api.utils.NMS;
+import net.minecraft.server.v1_8_R3.Block;
+import net.minecraft.server.v1_8_R3.PacketPlayOutNamedSoundEffect;
 import net.minecraft.server.v1_8_R3.PacketPlayOutWorldEvent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 import org.bukkit.Effect;
@@ -41,8 +43,14 @@ public class BlockRecordable implements Recordable {
                 NMS.updateBlock(player, shiftedLocation, Material.AIR, (byte) 0);
                 NMS.sendPacket(player, new PacketPlayOutWorldEvent(2001, NMS.toNMS(shiftedLocation), NMS.getCombined(material, data), false));
             } else {
-                // TODO Place sound
                 NMS.updateBlock(player, shiftedLocation, material, data);
+
+                Block.StepSound sound = Block.getById(material.getId()).stepSound;
+
+                NMS.sendPacket(player, new PacketPlayOutNamedSoundEffect(
+                        sound.getPlaceSound(), shiftedLocation.getX(), shiftedLocation.getY(), shiftedLocation.getZ(),
+                        (sound.getVolume1() + 1.0F) / 2.0F, sound.getVolume2() * 0.8F
+                ));
             }
         }
     }
