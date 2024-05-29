eaf0f9838b36787fe99a773e4bbcc69d857d1824
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-05-29 17:37:24 +1000

    add break sound

--- src/cf/pies/replay/api/recordable/world/BlockRecordable.java
+++ src/cf/pies/replay/api/recordable/world/BlockRecordable.java
@@ -4,9 +4,16 @@ import cf.pies.replay.api.Replay;
 import cf.pies.replay.api.ReplayPlayback;
 import cf.pies.replay.api.recordable.Recordable;
 import cf.pies.replay.api.utils.NMS;
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldEvent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.v1_8_R3.CraftSound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
 public class BlockRecordable implements Recordable {
     private Location location;
@@ -28,11 +35,14 @@ public class BlockRecordable implements Recordable {
 
     @Override
     public void play(ReplayPlayback playback) {
+        Location shiftedLocation = location.clone().add(playback.getOrigin());
         for (Player player : playback.getListeners()) {
             if (isBroken) {
-                NMS.updateBlock(player, location.clone().add(playback.getOrigin()), Material.AIR, (byte) 0);
+                NMS.updateBlock(player, shiftedLocation, Material.AIR, (byte) 0);
+                NMS.sendPacket(player, new PacketPlayOutWorldEvent(2001, NMS.toNMS(shiftedLocation), NMS.getCombined(material, data), false));
             } else {
-                NMS.updateBlock(player, location.clone().add(playback.getOrigin()), material, data);
+                // TODO Place sound
+                NMS.updateBlock(player, shiftedLocation, material, data);
             }
         }
     }
--- src/cf/pies/replay/api/utils/NMS.java
+++ src/cf/pies/replay/api/utils/NMS.java
@@ -20,21 +20,18 @@ public class NMS {
         return ((CraftPlayer) player).getHandle();
     }
 
-    public static void sendParticle(Player player, EnumParticle type, float x, float y, float z, float offsetX, float offsetY, float offsetZ, float speed, int count) {
-        EntityPlayer entityPlayer = getHandle(player);
-        PlayerConnection playerConnection = entityPlayer.playerConnection;
-        PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(type, true, x, y, z, offsetX, offsetY, offsetZ, speed, count);
-        playerConnection.sendPacket(packet);
-    }
-
-    public static IBlockData getIBlockData(Material material, int data) {
+    public static int getCombined(Material material, byte data) {
         int combined;
         if (data == 0) {
             combined = material.getId();
         } else {
             combined = material.getId() + (data << 12);
         }
-        return Block.getByCombinedId(combined);
+        return combined;
+    }
+
+    public static IBlockData getIBlockData(Material material, byte data) {
+        return Block.getByCombinedId(getCombined(material, data));
     }
 
     public static void updateBlock(Player player, Location location, Material material, byte data) {
