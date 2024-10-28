e20455a51c5047c1228b0444235c6a8f166e56c0
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-10-28 17:23:54 +1100

    Add comments to playback state

--- src/cf/pies/replay/api/enums/PlaybackState.java
+++ src/cf/pies/replay/api/enums/PlaybackState.java
@@ -1,8 +1,23 @@
 package cf.pies.replay.api.enums;
 
 public enum PlaybackState {
+    /**
+     * This state is used when the playback is initially setup.
+     */
     NONE,
+
+    /**
+     * Used when the playback is paused.
+     */
     PAUSED,
+
+    /**
+     * Used when the playback is currently playing.
+     */
     PLAYING,
+
+    /**
+     * Used if the replay is over.
+     */
     ENDED
 }
